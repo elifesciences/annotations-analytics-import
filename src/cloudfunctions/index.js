8d53c6197da1613b05852e93cae7aa12ec3fcc7c
Paul Shannon <p.shannon@elifesciences.org>
2017-12-06 10:26:23 +0000

    Add cloud function script for fetching annotations

---
+++ src/cloudfunctions/index.js
@@ -0,0 +1,19 @@
+var request = require('request');
+const Storage = require('@google-cloud/storage');
+const projectId = 'elife-analytics';
+const storage = Storage({
+  projectId: projectId,
+});
+
+exports.fetchAnnotations = function(req, res) {
+  request('https://hypothes.is/api/search?group=imRGyeeV', function (error, response, body) {
+
+    var annotationsBucket = storage.bucket('elife-annotations-export');
+    var file = annotationsBucket.file('annotations-generated.json');
+    file.save(body, function(err) {
+      if (!err){
+        res.send("file written successfully")
+      }
+    })
+  });
+};
